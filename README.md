11218f3755680210e3fdcf1f9ac4dfedd932cbf2
Jackbennett <jbennett@birkdalehigh.co.uk>
2018-06-12 14:48:29 +0100

    Initial Commit, working version but missing some commands
    Usefull commands but missing an overall coherance, see README To Do List

---
+++ README.md
@@ -0,0 +1,26 @@
+# Dynamic Learning User Data
+This is a set of commands to make the headache of managing Dynamic Learning user accounts easier with regards to class memberships and student details
+
+## Add-DLGroup
+* Adds an empty group header to the csv
+or
+* Adds a property of a group name to a user account, can then be piped to a csv
+
+## Format-DLForHumans
+Re-write the DL exported CSV with sensible column headers
+
+## Get-DLGroup
+Lists all groups found on the imported CSV
+
+## Update-DLUser
+Clear the users groups and re-assign "Yes" to their class and year group.
+
+# To Do List
+
+* `Update-User` should have test-user split off to remove compare-object output so it can be used to pipe further
+* Improve workflow to a solid import -> add -> edit -> export steps to upload to DL, currently we have separate but useful functions
+* Create `Export-DLUsers` that consumes `add-dlgroup`
+* Create `Import-DLUsers` to consume a CSV into something suitable for `Update-DLusers`
+* Finalize `New-DLUser` from reference.ps1 to be called in `update-DLUser` for adding new AD users to Dynamic learning to then have proper group assignment
+* `New-DLUser` needs to expose account password for distribution to students.
+* `Format-DLForHumans` should accept the same object about to be piped to `Export-DLUsers`
---
+++ reference.ps1
@@ -0,0 +1,96 @@
+﻿<#
+.SYNOPSIS
+    File kept for referencing when writing future additions
+.DESCRIPTION
+    Useful notes, ideas, attempts or one-lines used while creating the module.
+
+#>
+
+function Get-GroupMember{
+    Param(
+        # AD group class name or filter pattern
+        [string]
+        $Class
+    )
+
+    $g = get-adgroup -Filter {(name -like '*CS*') -or (name -like '*it*')} -SearchBase 'OU=Class Groups,OU=Student Groups,OU=Security Groups,OU=BHS,DC=BHS,DC=INTERNAL'
+
+    $g | where name -like $class | foreach {
+        $group = $psitem.name
+        Get-ADGroupMember $group |
+            get-aduser |
+            select SamAccountName, GivenName, Surname
+    }
+}
+
+function New-User
+{
+    <#
+    .Synopsis
+       Create a new DL user from and AD user
+    .DESCRIPTION
+       Pipe AD users into this to create a DL type user object ready to export as a CSV
+       Next you will want to assign group membership
+    #>
+    [CmdletBinding()]
+    [OutputType([psObject])]
+    Param
+    (
+        # S (for student) or TA (for Teacher)
+        [Parameter()]
+        [ValidateSet("S", "TA")]
+        [string]
+        $Type = "S"
+
+        , # User Name
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [ValidateLength(6,20)]
+        [string]
+        $SamAccountName
+
+        , # First Name
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [Alias('First Name', 'Firstname', 'First')]
+        [string]
+        $GivenName
+
+        , # Surname Name
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string]
+        $Surname
+
+        , # Email address
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string]
+        $EmailAddress
+
+        , # Used as the UPN field
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string]
+        $EmployeeNumber
+    )
+    Process
+    {
+        # Create a PS custom object to maintain the property order.
+        # Important when piped to ConvertTo-CSV as dynamic learning is specific
+        # in its column headers to upload.
+        # UPN is just the internal number to identify users accounts
+        # however SamAccountName is unique anyway.
+        [PSCustomObject] @{
+            '1 - Action' = 'A' # A for Add. E for Edit, D for Delete
+            '2 - User ID - do not edit (DL use only)' = ''
+            '3 - User Type' = $Type
+            '4 - User Name' = $SamAccountName
+            '5 - Password' = 'pass' + (Get-Random -Minimum 1000 -Maximum 9999)
+            '6 - Title' = ''
+            '7 - First name' = $GivenName
+            '8 - Middle name' = ''
+            '9 - Last name' = $Surname
+            '10 - DOB' = '01/01/1970'
+            '11 - Sex' = ''
+            '12 - UPN' = $EmployeeNumber
+            '13 - email' = $EmailAddress
+        }
+    }
+}
+
