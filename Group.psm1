3af8437243cf5e2cc5e84a17349e07d4623bb340
Jackbennett <jbennett@birkdalehigh.co.uk>
2019-01-18 13:42:42 +0000

    Run code formatting for presentation

--- Group.psm1
+++ Group.psm1
@@ -1,5 +1,5 @@
-﻿function Update-User{
-<#
+﻿function Update-User {
+    <#
 .EXAMPLE
     Get-ADGroup -SearchBase '<OU Path>' -Filter * |
         where name -like 7*_cs* |
@@ -27,29 +27,29 @@
     Just update the records for the members of 7m6_Cs.
     Exisitng members won't be removed because they are not found by the class selection.
 #>
-[CmdletBinding()]
-Param(
-    [Parameter(Mandatory=$true,
-               ValueFromPipeline=$true,
-               ValueFromPipelineByPropertyName=$true,
-               Position=0)]
-    [Alias('Name')]
-    [string[]]
-    $class
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)]
+        [Alias('Name')]
+        [string[]]
+        $class
 
-    , # Data file
-    [Parameter(Mandatory=$true,
-               ValueFromPipeline=$true,
-               ValueFromPipelineByPropertyName=$true,
-               Position=1)]
-    [string]
-    $CSV
-)
-    Begin{
+        , # Data file
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 1)]
+        [string]
+        $CSV
+    )
+    Begin {
         $c = Import-Csv $csv
     }
-    Process{
-        forEach($group in $class){
+    Process {
+        forEach ($group in $class) {
             Write-Verbose "Start to set members for group: $group"
             $adgroup = $group | Get-ADGroupMember | Select-Object -ExpandProperty samaccountname
 
@@ -58,18 +58,20 @@ Param(
             # Check
             $a = ($adgroup | Measure-Object).Count
             $b = ($DLGroup | Measure-Object).Count
-            if($a -ne $b){
+            if ($a -ne $b) {
                 Write-Output "<= In ADGroup, => in DLGroup. Missing for: $group"
                 Compare-object $adgroup $DLGroup.'4 - User name'
-            } else {
+            }
+            else {
                 Write-Verbose "Group has $a members in both AD and DL"
             }
 
             # Mark the use record as edited for re-upload
             $DLGroup | Foreach-Object {
-                if($_.'1 - Action' -ne 'A' ){
+                if ($_.'1 - Action' -ne 'A' ) {
                     $_.'1 - Action' = 'E'
-                } else {
+                }
+                else {
                     Write-Verbose "Not changing Add action to Edit."
                 }
             }
@@ -78,44 +80,45 @@ Param(
             [switch]$script:added = $False
             $DLGroup | Foreach-Object {
                 $_.psobject.properties | Foreach-Object {
-                    if($_.Name -Like "*$group*"){
+                    if ($_.Name -Like "*$group*") {
                         $_.Value = 'Yes'
                         $script:added = $True
-                    } elseif ($_.name -Like "* | *") {
+                    }
+                    elseif ($_.name -Like "* | *") {
                         $_.Value = "No"
                     }
                 }
             }
-            if(-not $script:added){
+            if (-not $script:added) {
                 Write-Warning "$group DL Group not found to add membership"
             }
 
             # Add to correct Year Group
             [switch]$script:correctYear = $False
             $DLGroup | Foreach-Object {
-                $intake = [int]$_.'4 - User name'.Substring(0,2)
+                $intake = [int]$_.'4 - User name'.Substring(0, 2)
                 # TODO: handle calculating the intake year, this method below might need -1 appending depending on the current year.
-                $yeargroup = @('Year 7','Year 8','Year 9','Year 10','Year 11')[(get-date).year - ($intake + 2000)]
+                $yeargroup = @('Year 7', 'Year 8', 'Year 9', 'Year 10', 'Year 11')[(get-date).year - ($intake + 2000)]
                 $_.psobject.properties | Foreach-Object {
-                    if($_.Name -Like "$yeargroup |*"){
+                    if ($_.Name -Like "$yeargroup |*") {
                         $_.Value = 'Yes'
                         $script:correctYear = $True
                     }
                 }
             }
-            if($script:correctYear -and -not $added){
+            if ($script:correctYear -and -not $added) {
                 Write-Warning "$group DL Group not found to add membership"
             }
 
             Write-Output $DLGroup
         }
     }
-    End{
+    End {
 
     }
 }
 
-function Add-Group{
+function Add-Group {
     <#
     .SYNOPSIS
         Add a group to Dynamic Learning
@@ -134,16 +137,16 @@ function Add-Group{
     #>
     param(
         # AD User to add group property to
-        [Parameter(Position=0,
-                   ParameterSetName="User",
-                   ValueFromPipeline,
-                   ValueFromPipelineByPropertyName)]
+        [Parameter(Position = 0,
+            ParameterSetName = "User",
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
         $User
 
         , # AD User to add group property to
-        [Parameter(Position=0,
-                   ParameterSetName="Dataset",
-                   ValueFromPipeline)]
+        [Parameter(Position = 0,
+            ParameterSetName = "Dataset",
+            ValueFromPipeline)]
         $CSV
 
         , # AD Class name
@@ -154,8 +157,8 @@ function Add-Group{
         [string]
         $DLUserName = "MR S Moss"
     )
-    Process{
-        switch($PSCmdlet.ParameterSetName){
+    Process {
+        switch ($PSCmdlet.ParameterSetName) {
             'User' {
                 $User | Add-Member -MemberType NoteProperty -Name "$Class | $DLUserName" -Value 'Yes' -PassThru -force
             }
@@ -168,7 +171,7 @@ function Add-Group{
     }
 }
 
-function Get-Group{
+function Get-Group {
     <#
     .SYNOPSIS
         Get Dynamic Learning groups from exported file
@@ -213,11 +216,10 @@ function Get-Group{
         "13 - Email"
     )
 
-    (Get-content $path -TotalCount 1).replace('"','') -split ',' | where {$_ -notin $DLHeaders}
+    (Get-content $path -TotalCount 1).replace('"', '') -split ',' | where {$_ -notin $DLHeaders}
 }
 
-function New-User
-{
+function New-User {
     <#
     .Synopsis
         Create a new DL user from and AD user
@@ -238,55 +240,54 @@ function New-User
         $Type = "S"
 
         , # User Name
-        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
-        [ValidateLength(6,20)]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateLength(6, 20)]
         [string]
         $SamAccountName
 
         , # First Name
-        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [Alias('First Name', 'Firstname', 'First')]
         [string]
         $GivenName
 
         , # Surname Name
-        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [string]
         $Surname
 
         , # Email address
-        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [string]
         $EmailAddress
 
         , # Used as the UPN field
-        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
         [string]
         $EmployeeNumber
     )
-    Process
-    {
+    Process {
         # Create a PS custom object to maintain the property order.
         # Important when piped to ConvertTo-CSV as dynamic learning is specific
         # in its column headers to upload.
         # UPN is just the internal number to identify users accounts
         # however SamAccountName is unique anyway.
         [PSCustomObject] @{
-            '1 - Action' = 'A' # A for Add. E for Edit, D for Delete
+            '1 - Action'                              = 'A' # A for Add. E for Edit, D for Delete
             '2 - User ID - do not edit (DL use only)' = ''
-            '3 - User Type' = $Type
-            '4 - User Name' = $SamAccountName
-            '5 - Password' = 'pass' + (Get-Random -Minimum 1000 -Maximum 9999)
-            '6 - Title' = ''
-            '7 - First name' = $GivenName
-            '8 - Middle name' = ''
-            '9 - Last name' = $Surname
-            '10 - DOB' = '01/01/1970'
-            '11 - Sex' = ''
-            '12 - UPN' = $EmployeeNumber
-            '13 - email' = $EmailAddress
+            '3 - User Type'                           = $Type
+            '4 - User Name'                           = $SamAccountName
+            '5 - Password'                            = 'pass' + (Get-Random -Minimum 1000 -Maximum 9999)
+            '6 - Title'                               = ''
+            '7 - First name'                          = $GivenName
+            '8 - Middle name'                         = ''
+            '9 - Last name'                           = $Surname
+            '10 - DOB'                                = '01/01/1970'
+            '11 - Sex'                                = ''
+            '12 - UPN'                                = $EmployeeNumber
+            '13 - email'                              = $EmailAddress
         }
     }
 }
@@ -309,11 +310,11 @@ function Get-MissingUser {
         [String]
         $SearchBase
     )
-    Begin{
-        $DL = Import-csv $CSV | Select-Object @{Name='SamAccountName';Expression= {$_.'4 - User Name'}}
+    Begin {
+        $DL = Import-csv $CSV | Select-Object @{Name = 'SamAccountName'; Expression = {$_.'4 - User Name'}}
     }
-    Process{
-        $ad = Get-ADUser -Filter {Enabled -eq $true} -Properties EmployeeNumber,EmailAddress -SearchBase $SearchBase
+    Process {
+        $ad = Get-ADUser -Filter {Enabled -eq $true} -Properties EmployeeNumber, EmailAddress -SearchBase $SearchBase
 
         Compare-Object $DL $AD -PassThru -Property SamAccountName | Where-Object SideIndicator -eq '=>'
     }
@@ -340,9 +341,8 @@ class DLUser {
 
 
     # Parameterless Constructor
-    DLUser ()
-    {
-        $this.action= 'A' # A for Add. E for Edit, D for Delete
+    DLUser () {
+        $this.action = 'A' # A for Add. E for Edit, D for Delete
         $this.type = 'S' # S or TA for Student or Teacher
         $this.password = 'pass' + (Get-Random -Minimum 1000 -Maximum 9999)
         $this.dob = '01/01/1970'
@@ -412,10 +412,10 @@ class DLUser {
 
     }
 
-    [DLUser] Delete(){
-         $this.action = 'D'
+    [DLUser] Delete() {
+        $this.action = 'D'
 
-         return $this
+        return $this
     }
 
     [PSCustomObject] Export() {return $this.Export(2)}
@@ -481,17 +481,17 @@ class DLUser {
         return [pscustomobject]
     }
 
-    [DLUser] AddGroup([string]$name){
-        $valid_group = $this::Groups.where({ $_.split('|')[0].trim() -eq $name })
-        if(-not $valid_group) {
+    [DLUser] AddGroup([string]$name) {
+        $valid_group = $this::Groups.where( { $_.split('|')[0].trim() -eq $name })
+        if (-not $valid_group) {
             Throw "Group $name is not found out of $($this::Groups.count) in ::Groups list"
         }
         # TODO: test if name containes | and switch search cases instead of throwing
-        if($valid_group.count -gt 1) {
+        if ($valid_group.count -gt 1) {
             Throw "Multiple matches for group($name) as: $valid_group"
         }
 
-        if($this.MemberOf -notcontains $valid_group){
+        if ($this.MemberOf -notcontains $valid_group) {
             # If this list actually needs modifing then set the edit attribute.
             $this.action = 'E'
             $this.MemberOf.Add($valid_group)
@@ -500,17 +500,17 @@ class DLUser {
         return $this
     }
 
-    [DLUser] RemoveGroup([string]$name){
-        $valid_group = $this::Groups.where({ $_.split('|')[0].trim() -eq $name })
-        if(-not $valid_group) {
+    [DLUser] RemoveGroup([string]$name) {
+        $valid_group = $this::Groups.where( { $_.split('|')[0].trim() -eq $name })
+        if (-not $valid_group) {
             Throw "Group $name is not found out of $($this::Groups.count) in ::Groups list"
         }
         # TODO: test if name containes | and switch search cases instead of throwing
-        if($valid_group.count -gt 1) {
+        if ($valid_group.count -gt 1) {
             Throw "Multiple matches for group($name) as: $valid_group"
         }
 
-        if($this.MemberOf -contains $valid_group){
+        if ($this.MemberOf -contains $valid_group) {
             # If this list actually needs modifing then set the edit attribute.
             $this.action = 'E'
             $this.MemberOf.Remove($valid_group)
@@ -534,14 +534,15 @@ function Import-User {
     #>
     [CmdletBinding()]
     param (
-        [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [ValidateNotNullOrEmpty()]
         $User
     )
     process {
-        if($PSBoundParameters.User){
+        if ($PSBoundParameters.User) {
             [DLUser]::new($User)
-        } else{
+        }
+        else {
             Write-Warning "No input"
         }
     }
@@ -559,7 +560,7 @@ function Export-User {
         [Parameter(Mandatory, ValueFromPipeline)]
         [DLUser[]]$User
     )
-    Process{
+    Process {
         $User.export()
     }
 }
@@ -581,18 +582,18 @@ function Set-GroupMember {
         [Parameter(Mandatory)]
         [string[]]$group
     )
-    Process{
+    Process {
         foreach ($g in $group) {
             $users_in_AD = get-adgroupmember $g | Add-Member -PassThru -MemberType AliasProperty -Name Username -Value SamAccountName -force
 
             $users_in_DL = $User | where-object {
                 # Where the user lists a DL group starting with the name of the AD Group, removing whitespace.
                 # DL Groups are <Group Name> | <Group Creator> for some reason in the csv.
-                $psitem.MemberOf.where({ $psitem.split('|')[0].trim().ToLower().StartsWith($g.ToLower()) })
+                $psitem.MemberOf.where( { $psitem.split('|')[0].trim().ToLower().StartsWith($g.ToLower()) })
             }
 
-            if($users_in_DL.count -eq 0){
-                $users_in_DL = @{username= $null}
+            if ($users_in_DL.count -eq 0) {
+                $users_in_DL = @{username = $null}
             }
 
             $comparison = Compare-Object $users_in_AD $users_in_DL -property username -PassThru
@@ -611,7 +612,7 @@ function Set-GroupMember {
                 "Group"   = $g
                 "Added"   = $added.count
                 "Removed" = $removed.count
-                "Total" = $users_in_DL.count + ($added.count - $removed.count)
+                "Total"   = $users_in_DL.count + ($added.count - $removed.count)
             }
             # TODO: If users in DL are Yes, check they're in AD, if no set to no
             # TODO: Set usernames from AD to yes in DL.
